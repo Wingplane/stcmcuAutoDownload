833f3db6761547e662b1abe20b6051b59719ee09
ZhangYue <zhangyue@petalmail.com>
2022-09-29 18:13:25 +0800

    更新功能并修复了部分问题
    # 增加了下载目录
    # 修复了部分按钮无法按下导致下载失败的问题

--- edgestcsignin/edge.py
+++ edgestcsignin/edge.py
@@ -2,9 +2,15 @@ import os
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.edge.options import Options
 
 my_username = "<username>"
 my_password = "<password>"
+download_path = "D:\\AutoDownload"
+prefs = {'profile.default_content_settings.popups': 0,
+         'download.default_directory': download_path,
+         "profile.default_content_setting_values.automatic_downloads": 1
+         }
 
 f = open('record.txt', encoding='utf-8')
 day = int(f.read())
@@ -19,26 +25,50 @@ f = open('website.txt', encoding='utf-8')
 webpage = []
 for line in f:
     webpage.append(line.strip())
-f.close
+f.close()
 
-driver = webdriver.Edge()
 
+def mkdir(path):
+    path = path.strip()
+    path = path.rstrip("\\")
+    isExists = os.path.exists(path)
+    if not isExists:
+        os.makedirs(path)
+        print(path + ' 创建成功')
+        return True
+    else:
+        print(path + ' 目录已存在')
+        return False
+
+
+mkdir(download_path)
+
+option = Options()
+option.add_experimental_option('prefs', prefs)
+option.add_argument('disable-blink-features=AutomationControlled')
+driver = webdriver.Edge(options=option)
 driver.get("https://www.stmcu.com.cn/")
 
 driver.find_element(By.LINK_TEXT, "登录").click()
 driver.find_element(By.LINK_TEXT, "账号密码登录").click()
 driver.find_element(By.ID, "username").send_keys(my_username)
 driver.find_element(By.ID, "password").send_keys(my_password)
-driver.find_element(By.CSS_SELECTOR, ".an_lan").click()
+driver.find_element(By.CLASS_NAME, "l1").click()
 
 js = ''
-for i in range((day-1) * 30, day * 30):
+print("以下网址下载失败")
+for i in range((day - 1) * 30, day * 30):
     js = 'window.open("' + webpage[i] + '");'
     driver.execute_script(js)
-    time.sleep(1)
+    time.sleep(10)
     handles = driver.window_handles
-    driver.switch_to.window(handles[-1])
-    driver.find_element(By.XPATH, '//*[@id="down_load_btn"]').click()
-    time.sleep(2)
+    try:
+        driver.switch_to.window(handles[-1])
+        time.sleep(4)
+        driver.find_element(By.XPATH, '//*[@id="down_load_btn"]').click()
+        time.sleep(4)
+    except:
+        print(webpage[i])
 time.sleep(10)
 driver.quit()
+print('下载已完成')
